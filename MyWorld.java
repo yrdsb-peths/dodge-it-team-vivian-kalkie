bb20b772f45f3e35ddbcd6618e5b7d7b74a52922
vivianli19 <341207876@gapps.yrdsb.ca>
2025-04-22 14:02:35 -0400

    adding man and banana to world

--- MyWorld.java
+++ MyWorld.java
@@ -3,5 +3,11 @@ import greenfoot.*;
 public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1);
+        
+        Man man = new Man();
+        addObject(man, 100, 100);
+        
+        Banana banana = new Banana();
+        addObject(banana, 600, 100);
     }
 }

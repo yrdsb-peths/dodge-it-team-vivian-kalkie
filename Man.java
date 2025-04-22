d424ed5792c3b3956a041e5bcff0896cb99694d3
Kalkie Eswarathasan <kalkiemithraa@gmail.com>
2025-04-22 14:04:07 -0400

    created an act

--- Man.java
+++ Man.java
@@ -1,8 +1,25 @@
 import greenfoot.*;  
+
 public class Man extends Actor
 {
+    boolean atBottom = true;
+    
     public void act()
     {
+      if(Greenfoot.mouseClicked(null))
+      {
+          atBottom = !atBottom;
+      }
       
+      if(atBottom)
+      {
+          setLocation(620, 620);
+      }
+      else 
+      {
+          setLocation(635, 635);
+      }
+    }
+}
     }
 }
